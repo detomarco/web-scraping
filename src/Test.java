ea28854382928bb2bcd56c3f2ae0158f4455a396
Marco De Toma <idp.detoma@gmail.com>
2015-12-15 16:58:37 +0100

    jaunt test

--- src/Test.java
+++ src/Test.java
@@ -1,9 +1,19 @@
+import com.jaunt.Element;
+import com.jaunt.Elements;
+import com.jaunt.JauntException;
+import com.jaunt.UserAgent;
 
 public class Test {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws JauntException {
+		UserAgent userAgent = new UserAgent();      								
+	    userAgent.settings.autoSaveAsHTML = true;
+	    userAgent.visit("http://www.marcodetoma.altervista.org");       				
+	    for(Element ad: userAgent.doc.findFirst("<head>").findEvery("<meta>")){
+	    	System.out.println(ad);
+	    	
+	    }
+	    System.out.println("end");
 	}
 
 }
