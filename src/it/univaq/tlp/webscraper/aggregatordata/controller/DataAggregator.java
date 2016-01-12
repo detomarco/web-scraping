3da6f9b00eb2212d29b4852fc781376b467d2cb7
Gianluca Filippone <filgianluca93@gmail.com>
2016-01-12 12:35:22 +0100

    ?

--- src/it/univaq/tlp/webscraper/aggregatordata/controller/DataAggregator.java
+++ src/it/univaq/tlp/webscraper/aggregatordata/controller/DataAggregator.java
@@ -1,6 +1,10 @@
 package it.univaq.tlp.webscraper.aggregatordata.controller;
 
+import java.util.List;
+import java.util.Map;
+
 import it.univaq.tlp.webscraper.aggregatordata.Storable;
+import it.univaq.tlp.webscraper.aggregatordata.StorageException;
 import it.univaq.tlp.webscraper.aggregatordata.model.AggregatedData;
 
 public class DataAggregator {
@@ -15,7 +19,14 @@ public class DataAggregator {
 		
 		String source = data.getSource();
 		
-	
+		List<Map<String, String>> results;
+		try{
+			results = storage.get("web_datas", "source = "+source);
+		} catch (StorageException e){
+			e.printStackTrace();
+			
+			return;
+		}
 		
 	}
 	
