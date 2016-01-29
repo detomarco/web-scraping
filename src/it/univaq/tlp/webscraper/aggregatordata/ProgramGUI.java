8a06b0cf56f5ba587bf27d98c32f8a81bbe4fd08
lamoichenzio <dott.derricoalessandro@gmail.com>
2016-01-29 13:18:57 +0100

    Aggiunta classe ErrorDialog

--- src/it/univaq/tlp/webscraper/aggregatordata/ProgramGUI.java
+++ src/it/univaq/tlp/webscraper/aggregatordata/ProgramGUI.java
@@ -1,20 +1,25 @@
 package it.univaq.tlp.webscraper.aggregatordata;
+import org.eclipse.swt.widgets.Shell;
+
 import it.univaq.tlp.webscraper.aggregatordata.repository.Storable;
 import it.univaq.tlp.webscraper.aggregatordata.repository.StorageException;
 import it.univaq.tlp.webscraper.aggregatordata.repository.database.MySQLDatabase;
 import it.univaq.tlp.webscraper.aggregatordata.view.UserInterface;
 import it.univaq.tlp.webscraper.aggregatordata.view.gui.GUI;
+import it.univaq.tlp.webscraper.aggregatordata.view.gui.ErrorDialog;;
 
-public class ProgramGUI {
 
-	public static void main(String[] args) {
+public class ProgramGUI extends Thread{
+
+	public static void main(String[] args) throws Exception {
 		try {
 			Storable storage = new MySQLDatabase("root", "root", "localhost", 3306, "web_scraper");
 			UserInterface console = new GUI(storage);
 			console.run();
 			
 		} catch (StorageException e) {
-			//ErrorDialog dialog = new ErrorDialog(parent, style);
+			ErrorDialog dialog = new ErrorDialog(new Shell(), 0);
+			sleep(5000);
 			e.printStackTrace();
 		}
 	
