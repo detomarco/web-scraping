ebf84b1ce97146e70ae185a7b0bae56c4433ab9d
Marco De Toma <idp.detoma@gmail.com>
2016-02-01 17:49:03 +0100

    fix

--- src/it/univaq/tlp/webscraper/WebScraper.java
+++ src/it/univaq/tlp/webscraper/WebScraper.java
@@ -1,10 +1,9 @@
 package it.univaq.tlp.webscraper;
 
-import it.univaq.tlp.webscraper.controller.database.Database;
+import it.univaq.tlp.webscraper.controller.Storable;
 import it.univaq.tlp.webscraper.controller.database.MySQLDatabase;
 import it.univaq.tlp.webscraper.controller.exception.StorageException;
 import it.univaq.tlp.webscraper.view.TUI;
-import it.univaq.tlp.webscraper.view.gui.GUI;
 import it.univaq.tlp.webscraper.view.UserInterface;
 
 @SuppressWarnings("unused")
@@ -12,7 +11,7 @@ public class WebScraper {
 
 	public static void main(String[] args) {
 		
-		Database storage = new MySQLDatabase("root", "root", "localhost", 3306, "web_scraper");
+		Storable storage = new MySQLDatabase("root", "root", "localhost", 3306, "web_scraper");
 		
 		try {
 			
